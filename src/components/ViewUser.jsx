fc4a0ecb2d937bf00c49d7976b1979bbd83d7d3e
Johann C <joe7hann@proton.me>
2023-06-28 14:24:07 -0500

    chore:component userView added

--- src/components/ViewUser.jsx
+++ src/components/ViewUser.jsx
@@ -1,1 +1,10 @@
-//johann
+export const ViewUser = () => {
+
+    return (
+        <>
+            <div className="container">
+                <h3>Perfil de Empleado</h3>
+            </div>
+        </>
+    )
+}
