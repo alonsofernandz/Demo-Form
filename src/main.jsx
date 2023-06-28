ea0c9fa5b41669a51487559cbdc2aa01e1494cfa
Luis vcn <khanccer@gmail.com>
2023-06-28 17:19:12 -0500

    Add FormOtrosDatos :P

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { FormLabolData } from './components/FormLaboData'
 import './index.css'
+import { FormOtrosDatos } from './components/FormOtrosDatos'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FormLabolData />
+    <FormOtrosDatos />
   </React.StrictMode>,
 )
