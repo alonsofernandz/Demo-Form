30f40aff4b55c9f41576d8ef711fbe80b1aa2ad9
Joan <jquispe@betriax.com>
2023-06-28 17:07:57 -0500

    buttons v1

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,1 +1,29 @@
-// joan
+
+
+export const Boton = ({ children }) => {
+  return (
+    <div className='d-inline-block mt-5'>
+      <button
+        className='btn btn-primary mx-auto d-block'
+        style={{ width: 'fit-content', backgroundColor: '#1e1e2d' }}
+      >
+        {children}
+      </button>
+      <div className='mt-3'>
+        <button
+          className='btn btn-info text-white'
+          style={{ width: 'fit-content', backgroundColor:"#009EF7"}}
+        >
+          {children}
+        </button>
+        <div className='bg-success d-inline-block rounded'>
+          <button className='btn btn-info text-white' 
+          style={{ width: 'fit-content', backgroundColor:"#50cd89"}}>
+            {children}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { FormLabolData } from './components/FormLaboData'
 import './index.css'
+import { Boton } from './components/Buttons'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <FormLabolData />
+    <Boton children={"prueba"}/>
   </React.StrictMode>,
 )
