2ee248b5c0a0f8b2464c43714f93c5d5c34de691
alonsofernandz <af5620676@gmail.com>
2023-06-28 14:39:10 -0500

    update bootstrap

--- src/components/FormLaboData.jsx
+++ src/components/FormLaboData.jsx
@@ -1,1 +1,13 @@
 //juan alonso
+
+export const FormLabolData = () => {
+    return(
+        <>
+            <div className="container">
+                <div className="card-body">
+                    <h2 className="">Datos Laborales</h2>
+                </div>
+            </div>
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { FormLabolData } from './components/FormLaboData'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <FormLabolData />
   </React.StrictMode>,
 )
